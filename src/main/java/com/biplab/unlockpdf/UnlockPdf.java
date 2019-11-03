b6048ad7ab9cfccd690e23d016db183ea7859920
Biplab Nayak <biplabnayak@Biplabs-MacBook-Air.local>
2019-11-03 18:35:32 +0530

    initial commit

---
+++ src/main/java/com/biplab/unlockpdf/UnlockPdf.java
@@ -0,0 +1,28 @@
+package com.biplab.unlockpdf;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+
+import java.io.File;
+
+/**
+ * Main class to unlock pdf
+ */
+public class UnlockPdf {
+
+    private static String FILE_NAME = "Aug 2019";
+    private static String FILE_PATH = "/Users/biplabnayak/Desktop/docs/creditcard";
+    private static String PASSWORD = "PASSWORD";
+    private static String EXT = ".pdf";
+
+    public static void main(String[] args) throws Exception {
+
+        File inFile = new File(FILE_PATH + File.pathSeparator + FILE_NAME + EXT);
+        File outFile = new File(FILE_PATH + File.pathSeparator + FILE_NAME + "_unlocked" + EXT);
+        PDDocument document = PDDocument.load(inFile, PASSWORD);
+
+        document.setAllSecurityToBeRemoved(true);
+
+        document.save(outFile);
+
+    }
+}
