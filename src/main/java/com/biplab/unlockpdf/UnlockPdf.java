e97de89ac31755b592b495c87b0f4a27aa4f0f91
Biplab Nayak <biplabnayak@Biplabs-MacBook-Air.local>
2019-11-03 18:48:29 +0530

    upgraded pdf box version

--- src/main/java/com/biplab/unlockpdf/UnlockPdf.java
+++ src/main/java/com/biplab/unlockpdf/UnlockPdf.java
@@ -11,13 +11,13 @@ public class UnlockPdf {
 
     private static String FILE_NAME = "Aug 2019";
     private static String FILE_PATH = "/Users/biplabnayak/Desktop/docs/creditcard";
-    private static String PASSWORD = "PASSWORD";
-    private static String EXT = ".pdf";
+    private static String PASSWORD = "DUMMY";
+    private static String EXT = ".PDF";
 
     public static void main(String[] args) throws Exception {
 
-        File inFile = new File(FILE_PATH + File.pathSeparator + FILE_NAME + EXT);
-        File outFile = new File(FILE_PATH + File.pathSeparator + FILE_NAME + "_unlocked" + EXT);
+        File inFile = new File(FILE_PATH + File.separator + FILE_NAME + EXT);
+        File outFile = new File(FILE_PATH + File.separator + FILE_NAME + "_unlocked" + EXT);
         PDDocument document = PDDocument.load(inFile, PASSWORD);
 
         document.setAllSecurityToBeRemoved(true);
